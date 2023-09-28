3dc2b12226c216bd03fbd6ee72306dd7afa69417
Shimamura Yuta <shimamura@agora-c.com>
2023-09-28 17:32:06 +0900

    20230928 commit

--- lib/screens/record_settings.dart
+++ lib/screens/record_settings.dart
@@ -40,100 +40,148 @@ class _RecordSettingsScreenState extends State<RecordSettingsScreen> {
                 ],
               ),
               const SizedBox(height: 8),
-              Container(
-                decoration: const BoxDecoration(
-                  border: Border(bottom: BorderSide(color: kGreyColor)),
-                ),
-                padding: const EdgeInsets.all(8),
-                child: const Row(
+              SingleChildScrollView(
+                child: Column(
                   children: [
-                    Icon(FluentIcons.calculator),
-                    SizedBox(width: 8),
-                    Text(
-                      '帳票出力時の時間の計算に使う設定',
-                      style: TextStyle(fontSize: 18),
+                    Container(
+                      decoration: const BoxDecoration(
+                        border: Border(bottom: BorderSide(color: kGreyColor)),
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: const Row(
+                        children: [
+                          Icon(FluentIcons.calculator),
+                          SizedBox(width: 8),
+                          Text(
+                            '帳票出力時の時間の計算に使う設定',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    InfoLabel(
+                      label: '法定労働時間',
+                      child: ComboBox<String>(
+                        value: '8時間',
+                        items: const [
+                          ComboBoxItem(
+                            value: '8時間',
+                            child: Text('8時間'),
+                          ),
+                        ],
+                        onChanged: (value) {},
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    InfoLabel(
+                      label: '出勤時間の丸め方',
+                      child: ComboBox<String>(
+                        value: '切捨',
+                        items: const [
+                          ComboBoxItem(
+                            value: '切捨',
+                            child: Text('切捨'),
+                          ),
+                        ],
+                        onChanged: (value) {},
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    InfoLabel(
+                      label: '出勤時間のまるめ分数',
+                      child: ComboBox<String>(
+                        value: '1分',
+                        items: const [
+                          ComboBoxItem(
+                            value: '1分',
+                            child: Text('1分'),
+                          ),
+                        ],
+                        onChanged: (value) {},
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    InfoLabel(
+                      label: '退勤時間の丸め方',
+                      child: ComboBox<String>(
+                        value: '切捨',
+                        items: const [
+                          ComboBoxItem(
+                            value: '切捨',
+                            child: Text('切捨'),
+                          ),
+                        ],
+                        onChanged: (value) {},
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Expander(
+                      header: Text('法定労働時間'),
+                      content: Column(
+                        children: [],
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Expander(
+                      header: Text('時間のまるめ'),
+                      content: Column(
+                        children: [],
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Expander(
+                      header: Text('所定労働時間帯'),
+                      content: Column(
+                        children: [],
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Expander(
+                      header: Text('深夜時間帯'),
+                      content: Column(
+                        children: [],
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Expander(
+                      header: Text('休日指定(曜日)'),
+                      content: Column(
+                        children: [],
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Expander(
+                      header: Text('休日指定(日付)'),
+                      content: Column(
+                        children: [],
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Container(
+                      decoration: const BoxDecoration(
+                        border: Border(bottom: BorderSide(color: kGreyColor)),
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: const Row(
+                        children: [
+                          Icon(FluentIcons.calculator),
+                          SizedBox(width: 8),
+                          Text(
+                            '打刻時の設定',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Expander(
+                      header: Text('休憩時間の自動付与'),
+                      content: Column(
+                        children: [],
+                      ),
                     ),
                   ],
-                ),
-              ),
-              const SizedBox(height: 8),
-              InfoLabel(
-                label: '法定労働時間',
-                child: ComboBox<String>(
-                  value: '8時間',
-                  items: const [
-                    ComboBoxItem(
-                      child: Text('8時間'),
-                      value: '8時間',
-                    ),
-                  ],
-                  onChanged: (value) {},
-                ),
-              ),
-              const SizedBox(height: 8),
-              const Expander(
-                header: Text('法定労働時間'),
-                content: Column(
-                  children: [],
-                ),
-              ),
-              const SizedBox(height: 8),
-              const Expander(
-                header: Text('時間のまるめ'),
-                content: Column(
-                  children: [],
-                ),
-              ),
-              const SizedBox(height: 8),
-              const Expander(
-                header: Text('所定労働時間帯'),
-                content: Column(
-                  children: [],
-                ),
-              ),
-              const SizedBox(height: 8),
-              const Expander(
-                header: Text('深夜時間帯'),
-                content: Column(
-                  children: [],
-                ),
-              ),
-              const SizedBox(height: 8),
-              const Expander(
-                header: Text('休日指定(曜日)'),
-                content: Column(
-                  children: [],
-                ),
-              ),
-              const SizedBox(height: 8),
-              const Expander(
-                header: Text('休日指定(日付)'),
-                content: Column(
-                  children: [],
-                ),
-              ),
-              const SizedBox(height: 16),
-              Container(
-                decoration: const BoxDecoration(
-                  border: Border(bottom: BorderSide(color: kGreyColor)),
-                ),
-                padding: const EdgeInsets.all(8),
-                child: const Row(
-                  children: [
-                    Icon(FluentIcons.calculator),
-                    SizedBox(width: 8),
-                    Text(
-                      '打刻時の設定',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 8),
-              const Expander(
-                header: Text('休憩時間の自動付与'),
-                content: Column(
-                  children: [],
                 ),
               ),
             ],
