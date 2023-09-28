36fec1aedb6031f6e31ecbccfd4dc680f929c778
Shimamura Yuta <shimamura@agora-c.com>
2023-09-28 16:13:31 +0900

    20230928 commit

--- lib/screens/record_settings.dart
+++ lib/screens/record_settings.dart
@@ -40,10 +40,101 @@ class _RecordSettingsScreenState extends State<RecordSettingsScreen> {
                 ],
               ),
               const SizedBox(height: 8),
-              ListView(
-                children: const [
-                  Text('帳票などの出力時、時間'),
-                ],
+              Container(
+                decoration: const BoxDecoration(
+                  border: Border(bottom: BorderSide(color: kGreyColor)),
+                ),
+                padding: const EdgeInsets.all(8),
+                child: const Row(
+                  children: [
+                    Icon(FluentIcons.calculator),
+                    SizedBox(width: 8),
+                    Text(
+                      '帳票出力時の時間の計算に使う設定',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 8),
+              InfoLabel(
+                label: '法定労働時間',
+                child: ComboBox<String>(
+                  value: '8時間',
+                  items: const [
+                    ComboBoxItem(
+                      child: Text('8時間'),
+                      value: '8時間',
+                    ),
+                  ],
+                  onChanged: (value) {},
+                ),
+              ),
+              const SizedBox(height: 8),
+              const Expander(
+                header: Text('法定労働時間'),
+                content: Column(
+                  children: [],
+                ),
+              ),
+              const SizedBox(height: 8),
+              const Expander(
+                header: Text('時間のまるめ'),
+                content: Column(
+                  children: [],
+                ),
+              ),
+              const SizedBox(height: 8),
+              const Expander(
+                header: Text('所定労働時間帯'),
+                content: Column(
+                  children: [],
+                ),
+              ),
+              const SizedBox(height: 8),
+              const Expander(
+                header: Text('深夜時間帯'),
+                content: Column(
+                  children: [],
+                ),
+              ),
+              const SizedBox(height: 8),
+              const Expander(
+                header: Text('休日指定(曜日)'),
+                content: Column(
+                  children: [],
+                ),
+              ),
+              const SizedBox(height: 8),
+              const Expander(
+                header: Text('休日指定(日付)'),
+                content: Column(
+                  children: [],
+                ),
+              ),
+              const SizedBox(height: 16),
+              Container(
+                decoration: const BoxDecoration(
+                  border: Border(bottom: BorderSide(color: kGreyColor)),
+                ),
+                padding: const EdgeInsets.all(8),
+                child: const Row(
+                  children: [
+                    Icon(FluentIcons.calculator),
+                    SizedBox(width: 8),
+                    Text(
+                      '打刻時の設定',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 8),
+              const Expander(
+                header: Text('休憩時間の自動付与'),
+                content: Column(
+                  children: [],
+                ),
               ),
             ],
           ),
